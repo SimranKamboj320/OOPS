06436d9ec8721dbfe1367be74d990a15552dc28f
SimranKamboj320 <simrankamboj320@gmail.com>
2024-06-04 01:42:03 +0530

    OOPS
    Object oriented programming

---
+++ OOPS.java
@@ -0,0 +1,25 @@
+public class OOPs {
+    public static void main(String args[]) {
+        Pen p1 = new Pen(); //constructor // created a pen object called p1
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.color=("Yellow");
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+
+    }
+}
