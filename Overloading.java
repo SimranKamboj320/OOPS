4df892bcd7db5e50a904a9611718244a98d3bbbf
SimranKamboj320 <simrankamboj320@gmail.com>
2024-06-04 01:48:36 +0530

    Overloading Method
    Overloading Method (Polymorphism type)

---
+++ Overloading.java
@@ -0,0 +1,23 @@
+public class OOPS {
+
+    public static void main(String args[]) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.sum(1, 2));
+        System.out.println(cal.sum((float)1.5, (float)2));
+        System.out.println(cal.sum(1, 2, 3));
+    }
+}
+
+class Calculator {
+    int sum(int a, int b){
+        return a+b;
+    }
+
+    float sum(float a, float b){
+        return a+b;
+    }
+
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
